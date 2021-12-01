2ca0e48a713b8ac1787e051b75c3f52dce73aee8
Diego Revilla <string.diego.revilla@gmail.com>
2021-12-01 12:48:32 -0600

    Corrigiendo errores de reglas gramaticales

--- C-0/src/Analizadores/sym.java
+++ C-0/src/Analizadores/sym.java
@@ -8,44 +8,40 @@ package Analizadores;
 /** CUP generated class containing symbol constants. */
 public class sym {
   /* terminals */
-  public static final int AND = 16;
-  public static final int MENOR = 11;
-  public static final int ELSE = 24;
-  public static final int LLLAVE = 19;
-  public static final int CADENA = 18;
-  public static final int INT = 27;
-  public static final int OR = 15;
-  public static final int RPAREN = 6;
-  public static final int IGUAL = 13;
-  public static final int WHILE = 23;
-  public static final int DIVISION = 9;
-  public static final int SUMA = 8;
-  public static final int IF = 22;
-  public static final int LPAREN = 5;
-  public static final int ID = 30;
-  public static final int CADENATEXTO = 32;
-  public static final int ASIGNAR = 17;
-  public static final int DISTINTO = 14;
-  public static final int MAYOR = 12;
+  public static final int AND = 14;
+  public static final int MENOR = 9;
+  public static final int ELSE = 22;
+  public static final int LLLAVE = 17;
+  public static final int CADENA = 16;
+  public static final int INT = 25;
+  public static final int OR = 13;
+  public static final int RPAREN = 4;
+  public static final int IGUAL = 11;
+  public static final int WHILE = 21;
+  public static final int DIVISION = 7;
+  public static final int SUMA = 6;
+  public static final int IF = 20;
+  public static final int LPAREN = 3;
+  public static final int ID = 28;
+  public static final int CADENATEXTO = 30;
+  public static final int ASIGNAR = 15;
+  public static final int DISTINTO = 12;
+  public static final int MAYOR = 10;
   public static final int EOF = 0;
-  public static final int MAIN = 21;
-  public static final int BREAK = 28;
-  public static final int PUTW = 25;
-  public static final int NUMERO = 29;
+  public static final int MAIN = 19;
+  public static final int BREAK = 26;
+  public static final int PUTW = 23;
+  public static final int NUMERO = 27;
   public static final int error = 1;
-  public static final int PUTS = 26;
-  public static final int PTOCOMA = 4;
-  public static final int PRODUCTO = 7;
-  public static final int ENTERO = 31;
-  public static final int PARENTIZQ = 2;
-  public static final int RLLAVE = 20;
-  public static final int PARENTDER = 3;
-  public static final int RESTA = 10;
+  public static final int PUTS = 24;
+  public static final int PTOCOMA = 2;
+  public static final int PRODUCTO = 5;
+  public static final int ENTERO = 29;
+  public static final int RLLAVE = 18;
+  public static final int RESTA = 8;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
-  "PARENTIZQ",
-  "PARENTDER",
   "PTOCOMA",
   "LPAREN",
   "RPAREN",
